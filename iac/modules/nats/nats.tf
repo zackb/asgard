12df2813f9a4c0c5583fc36fe573b108968b566e
Zack Bartel <zack@bartel.com>
2020-05-12 21:34:51 -0700

    helm repo deprecated

--- iac/modules/nats/nats.tf
+++ iac/modules/nats/nats.tf
@@ -1,32 +1,29 @@
-data "helm_repository" "stable" {
-  name     = "nats"
-  url      = "https://nats-io.github.io/k8s/helm/charts/"
-}
 
 resource "helm_release" "nats" {
-  name      = "nats"
-  namespace = var.namespace
-  chart     = "nats/nats"
-  version   = "0.3.8"
+  name       = "nats"
+  repository = "https://nats-io.github.io/k8s/helm/charts/"
+  namespace  = var.namespace
+  chart      = "nats"
+  version    = "0.3.8"
 
   set {
-    name = "cluster.enabled"
+    name  = "cluster.enabled"
     value = true
   }
 
   # these are not arm
   set {
-    name = "reloader.enabled"
+    name  = "reloader.enabled"
     value = false
   }
 
   set {
-    name = "exporter.enabled"
+    name  = "exporter.enabled"
     value = false
   }
 
   set {
-    name = "natsbox.enabled"
+    name  = "natsbox.enabled"
     value = false
   }
 }
