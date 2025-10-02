0753a76637c42524ea31170e9b64ffc0a6e5466c
Zack Bartel <zack@bartel.com>
2025-10-01 19:42:46 -0700

    k8s secret for enc key

--- iac/modules/echovault/echovault.tf
+++ iac/modules/echovault/echovault.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.2"
+    }
   }
 }
 
@@ -23,12 +27,33 @@ resource "kubernetes_secret" "jwks" {
   type = "Opaque"
 }
 
+# Generate a 32-byte (256-bit) key for AES-256
+resource "random_password" "encryption_key" {
+  length  = 32
+  special = false
+}
+
+# Store encryption key as a secret
+resource "kubernetes_secret" "encryption_key" {
+  metadata {
+    name      = "echovault-encryption-key"
+    namespace = "default"
+  }
+
+  data = {
+    # base64 encode the key so it can be stored as stringData/data
+    encryptionKey = random_password.encryption_key.result
+  }
+
+  type = "Opaque"
+}
+
 
 resource "helm_release" "echovault" {
   name      = "echovault"
   namespace = var.namespace
   chart     = "../../echovault/deployments/helm/echovault"
-  version   = "0.0.3"
+  version   = "0.0.4"
   timeout   = 600
 
   values = [
@@ -51,7 +76,8 @@ resource "helm_release" "echovault" {
         replicas         = var.replicas
         jwksSecretName   = kubernetes_secret.jwks.metadata[0].name
         dbSecretName     = var.db_secret_name
-        encryptionKey    = "Z4fD6g8H9jK3mN5pQ2sV7xY1zB4eF6gH"
+        # encryptionKey           = "Z4fD6g8H9jK3mN5pQ2sV7xY1zB4eF6gH"
+        encryptionKeySecretName = kubernetes_secret.encryption_key.metadata[0].name
         minio = {
           enabled        = var.minio.endpoint != ""
           endpoint       = var.minio.endpoint
